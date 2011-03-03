e380a394aa639a06107128d5cf2d172f213af84b
Eric <eric@ubuntu.(none)>
2011-03-03 17:03:37 +0800

    translated fb:callback.php

--- app/controllers/fb_controller.rb
+++ app/controllers/fb_controller.rb
@@ -1,114 +1,116 @@
+require 'openssl'
+
 class FbController < ApplicationController
   def callback
 
-	logger.debug params.inspect
+	logger.debug "debug"+params.inspect
 	
-return
-=begin
-	@api_key = '195039077183959'
-	@secret = 'b6214e708bc46f37877b560f09b8cd1d'
+	api_key = '195039077183959'
+	secret = 'b6214e708bc46f37877b560f09b8cd1d'
 
 	# prepare the return data array
 	#$data = array('content' => array());
-
-	request = parse_signed_request($_REQUEST['signed_request'], secret);
+	data = Hash.new	
 
 	# parse signed data
+	request = parse_signed_request(params[:signed_request], secret);
 
 	if request.nil?
   		#handle an unauthenticated request here
+			logger.error "Unauthenticated Request!"
 	end
 
-	@payload = $request['credits']
+	payload = request['credits']
 
 	#retrieve all params passed in
+	func = params[:method]
+	order_id = payload['order_id']
 
-	@func = $_REQUEST['method']
-	@order_id = @payload['order_id']
-
-	if @func == 'payments_status_update'
-  		@status = @payload['status']
+	if func == 'payments_status_update'
+		data["content"] = Hash.new
+		status = payload['status']
 
  		#write your logic here, determine the state you wanna move to
-  		if @status == 'placed'
-    			@next_state = 'settled'
-    			@data['content']['status'] = @next_state
-  		end
+		if status == 'placed'
+			next_state = 'settled'
+	 		data['content']['status'] = next_state
+		end
   
 	#compose returning data array_change_key_case
-  	@data['content']['order_id'] = @order_id;
+  	data['content']['order_id'] = order_id
 
-	elsif @func == 'payments_get_items'
-  		# remove escape characters  
-  
-		@order_info = stripcslashes(@payload['order_info'])
-  		@item = json_decode(@order_info, true)
-  		@item[:price] = @item[:price].to_i
+	elsif func == 'payments_get_items'
+  	# remove escape characters  
+		order_info = payload['order_info'].tr("\\","")
+  	##item = ActiveSupport::JSON.decode(@order_info)
+		item = Product.find(order_info.to_i).attributes
+  	##item.price = item.price.to_i
 
-  		# for url fields, if not prefixed by http://, prefix them
-  		@url_key = array('product_url', 'image_url');  
-  		foreach ($url_key as $key) {
-    			if (substr($item[$key], 0, 7) != 'http://') {
-       				$item[$key] = 'http://'.$item[$key];
-    			}
-  		}
+		# for url fields, if not prefixed by http://, prefix them
+		#@url_key = array('product_url', 'image_url');  
+		#foreach ($url_key as $key) {
+  	#		if (substr($item[$key], 0, 7) != 'http://') {
+    # 				$item[$key] = 'http://'.$item[$key];
+  	#		}
+		#}
+		item["product_url"] = "http://ons.heroku.com"
+		item["image_url"] = "http://"
    
-  		# prefix test-mode
-  		if (isset($payload['test_mode'])) {
-  			  $update_keys = array('title', 'description');
-    			foreach ($update_keys as $key) {
-      				$item[$key] = '[Test Mode] '.$item[$key];
-    			}
-  		}
+  	# prefix test-mode
+  	if payload['test_mode'] != nil
+			#$update_keys = array('title', 'description');
+			#foreach ($update_keys as $key) {
+  		#		$item[$key] = '[Test Mode] '.$item[$key];
+			#}
+			item["title"] = "[Test Mode] " + item["title"]
+			item["description"] = "[Test Mode] " + item["description"]
+			
+		}
 
-  		$data['content'] = array($item);
+		data["content"] = Array.new
+		data['content'][0] = item
 	end
 
 # required by api_fetch_response()
-@data['method'] = @func
+data['method'] = func
 
 # send data back
-echo json_encode(@data);
-=end
+render :text => ActiveSupport::JSON.encode(data)
+
 end
 
 private
 
-def parse_signed_request(signed_request, secret) 
+	def parse_signed_request(signed_request, secret) 
 
+		encoded_sig, payload = signed_request.split('.', 2)
 
- @encoded_sig, @payload = signed_request.split('.', 2)
+		 # decode the data
+		sig = base64_url_decode(encoded_sig)
+		data = ActiveSupport::JSON.decode(base64_url_decode(payload))
 
-   # decode the data
-  @sig = base64_url_decode(encoded_sig);
-  @data = json_decode(base64_url_decode(payload), true);
+		if data['algorithm'].upcase != 'HMAC-SHA256'
+			logger.error 'Unknown algorithm. Expected HMAC-SHA256'
+			return nil
+		end
 
-	if @data['algorithm'].upcase != 'HMAC-SHA256'
-		logger.error 'Unknown algorithm. Expected HMAC-SHA256'
-		return false
+		# check signature
+
+		expected_sig = OpenSSL::HMAC.digest('sha256', secret, payload)
+
+		if sig != expected_sig 
+				logger.error 'Bad Signed JSON signature!'
+				return nil
+		end
+
+		return data;
+
 	end
 
 
-	# check signature
-  
-	@expected_sig = hash_hmac('sha256', @payload, @secret, @raw = true);
-  
-	if @sig != @expected_sig 
-    		logger.error 'Bad Signed JSON signature!'
-    		return false
-  	end
-
-  return @data;
-
-
-end
-
-def base64_url_decode(input)
-	return base64_decode(input.gsub('-_','+/')) 
-
-end
-
-
-
+	def base64_url_decode(input)
+		input += '=' * (4 - str.length.modulo(4))
+		return input.tr('-_','+/').unpack('m')[0] 
+	end
 
 end
