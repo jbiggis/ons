91662f963ca481c935b9e0c459611ae8a17b8f3e
Jonathan <jonathan@ubuntu.(none)>
2011-03-06 03:00:53 -0500

    moved hunter in fb controller

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -2,7 +2,7 @@ class Hunter < ActiveRecord::Base
 
 set_primary_key :hunter_id
 
-attr_accessible :hunter_id, :first_name, :last_name, :email, :gender, :DOB, :access_token
+attr_accessible :hunter_id, :first_name, :last_name, :email, :gender, :DOB, :access_token, :credits_left
 
 has_many :targets, :dependent => :destroy
 has_many :orders, :dependent => :destroy
