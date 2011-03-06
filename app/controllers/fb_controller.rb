ce4b2990fffab80b2349e7c53a58437e226f70cc
Jonathan <jonathan@ubuntu.(none)>
2011-03-06 04:40:47 -0500

    added condition to redirect to root_url after credit purchase

--- app/controllers/fb_controller.rb
+++ app/controllers/fb_controller.rb
@@ -5,105 +5,110 @@ class FbController < ApplicationController
 
   def callback
 
-puts "DEBUG-params:"+params.inspect
+	if params[:signed_request].nil? && params[:status] == 'settled'
+		redirect_to root_url
+		return
+	end
+
+	puts "DEBUG-params:"+params.inspect
+
+		# prepare the return data array
+		#$data = array('content' => array());
+		data = Hash.new	
+
+		# parse signed data
+		request = parse_signed_request(params[:signed_request], SECRET);
+
+		if request.nil?
+	  		#handle an unauthenticated request here
+			logger.error "Unauthenticated Request!"
+		end
+
+		payload = request['credits']
+
+		#retrieve all params passed in
+		func = params[:method]
+		order_id = payload['order_id']
+	puts "DEBUG-func:"+func
+	puts "DEBUG-orderid:"+order_id.to_s
+	puts "DEBUG-payload"+payload.inspect
+
+		if func == 'payments_status_update'
+			data["content"] = Hash.new
+			status = payload['status']		
+			order_details=ActiveSupport::JSON.decode(payload['order_details'])
+	puts "DEBUG-order_details:"+order_details.inspect
+			hunter = Hunter.find(order_details['buyer'].to_s) 
+	 		
+			#write your logic here, determine the state you wanna move to
+			if status == 'placed'
+
+				item = order_details['items'][0]
+	puts "DEBUG-order_details['items'][0]:"+order_details['items'][0].inspect
+
+				hunter.orders.create!(:order_id => order_id.to_s, :product_id=>item['item_id'], :status => status)
+
+				next_state = 'settled'
+		 		data['content']['status'] = next_state
+			end
+
+			if status == 'settled'
+				order = Order.find(order_id.to_s)
+				order.update_attributes(:status => status)
+		
+				product = Product.find(order.product_id)	
+				credits_left = hunter.credits_left 
+				total_credits = hunter.total_credits	
+				credits_left += product.credits_to_add
+				total_credits += product.credits_to_add
 	
-	# prepare the return data array
-	#$data = array('content' => array());
-	data = Hash.new	
 
-	# parse signed data
-	request = parse_signed_request(params[:signed_request], SECRET);
+				hunter.update_attributes(:credits_left => credits, :total_credits => total_credits)		
+	
+			end
+	  
+		#compose returning data array_change_key_case
+	  	data['content']['order_id'] = order_id
 
-	if request.nil?
-  		#handle an unauthenticated request here
-		logger.error "Unauthenticated Request!"
-	end
+		elsif func == 'payments_get_items'
+	  	# remove escape characters  
+			order_info = payload['order_info'].tr("\\","")
+	  	##item = ActiveSupport::JSON.decode(@order_info)
+			item = Product.find(order_info.to_i).attributes
+	  	##item.price = item.price.to_i
 
-	payload = request['credits']
-
-	#retrieve all params passed in
-	func = params[:method]
-	order_id = payload['order_id']
-puts "DEBUG-func:"+func
-puts "DEBUG-orderid:"+order_id.to_s
-puts "DEBUG-payload"+payload.inspect
-
-	if func == 'payments_status_update'
-		data["content"] = Hash.new
-		status = payload['status']		
-		order_details=ActiveSupport::JSON.decode(payload['order_details'])
-puts "DEBUG-order_details:"+order_details.inspect
-		hunter = Hunter.find(order_details['buyer'].to_s) 
- 		
-		#write your logic here, determine the state you wanna move to
-		if status == 'placed'
-
-			item = order_details['items'][0]
-puts "DEBUG-order_details['items'][0]:"+order_details['items'][0].inspect
-
-			hunter.orders.create!(:order_id => order_id.to_s, :product_id=>item['item_id'], :status => status)
-
-			next_state = 'settled'
-	 		data['content']['status'] = next_state
-		end
-
-		if status == 'settled'
-			order = Order.find(order_id.to_s)
-			order.update_attributes(:status => 'settled')
-			
-			product = Product.find(order.product_id)	
-			credits_left = hunter.credits_left 
-			total_credits = hunter.total_credits	
-			credits_left += product.credits_to_add
-			total_credits += product.credits_to_add
-		
-
-			hunter.update_attributes(:credits_left => credits, :total_credits => total_credits)		
-		
-		end
-  
-	#compose returning data array_change_key_case
-  	data['content']['order_id'] = order_id
-
-	elsif func == 'payments_get_items'
-  	# remove escape characters  
-		order_info = payload['order_info'].tr("\\","")
-  	##item = ActiveSupport::JSON.decode(@order_info)
-		item = Product.find(order_info.to_i).attributes
-  	##item.price = item.price.to_i
-
-		# for url fields, if not prefixed by http://, prefix them
-		#@url_key = array('product_url', 'image_url');  
-		#foreach ($url_key as $key) {
-  		#	if (substr($item[$key], 0, 7) != 'http://') {
-     		#		$item[$key] = 'http://'.$item[$key];
-  		#	}
-		#}
-		item["product_url"] = "http://ons.heroku.com"
-		item["image_url"] = "http://"
-   		item["item_id"] = order_info
-
-	  	# prefix test-mode
-	  	if payload['test_mode'] != nil
-			#$update_keys = array('title', 'description');
-			#foreach ($update_keys as $key) {
-	  		#		$item[$key] = '[Test Mode] '.$item[$key];
+			# for url fields, if not prefixed by http://, prefix them
+			#@url_key = array('product_url', 'image_url');  
+			#foreach ($url_key as $key) {
+	  		#	if (substr($item[$key], 0, 7) != 'http://') {
+	     		#		$item[$key] = 'http://'.$item[$key];
+	  		#	}
 			#}
-			item["title"] = "[Test Mode] " + item["title"]
-			item["description"] = "[Test Mode] " + item["description"]
-			
+			item["product_url"] = "http://ons.heroku.com"
+			item["image_url"] = "http://"
+	   		item["item_id"] = order_info
+
+		  	# prefix test-mode
+		  	if payload['test_mode'] != nil
+				#$update_keys = array('title', 'description');
+				#foreach ($update_keys as $key) {
+		  		#		$item[$key] = '[Test Mode] '.$item[$key];
+				#}
+				item["title"] = "[Test Mode] " + item["title"]
+				item["description"] = "[Test Mode] " + item["description"]
+		
+			end
+
+			data["content"] = Array.new
+			data['content'][0] = item
 		end
 
-		data["content"] = Array.new
-		data['content'][0] = item
-	end
+	# required by api_fetch_response()
+	data['method'] = func
 
-# required by api_fetch_response()
-data['method'] = func
-
-# send data back
-render :text => ActiveSupport::JSON.encode(data)
-
+	# send data back
+	render :text => ActiveSupport::JSON.encode(data)
+	
 end
 
 private
