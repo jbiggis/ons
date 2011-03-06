4c2287490a2d6fdca3eb30e8c58458eb0a62d14e
Eric <eric@ubuntu.(none)>
2011-03-07 02:01:14 +0800

    tidying up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,26 +9,4 @@ class HomeController < ApplicationController
 
 		end
 	end
-=begin	
-	def testes
-		##logger.debug "debugger" + params[:data]["31"][:name]	
-	debugstring = ""
-		params[:data].each do |person|
-			debugstring += person[1][:name]
-		end
-		logger.debug debugstring
-
-		logger.debug params.inspect
-		@targets = Target.find_by_hunter_id(params[:fb_id].to_i)
-		logger.debug @targets.inspect
-#		render :partial => 'target', :object => @targets
-		render :text => @targets.target_id
-		
-	
-	end
-
-	def add
-		logger.debug params.inspect
-	end
-=end
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -8,4 +8,7 @@ attr_accessible :target_id, :name, :matched_at, :notified_at
 
 default_scope :order => 'targets.name'
 
+validates :target_id, :presence => true
+validates :hunter_id, :presence => true
+
 end
