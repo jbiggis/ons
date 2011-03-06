6c231e3483aa34dce903295553132f80f407e845
Jonathan <jonathan@ubuntu.(none)>
2011-03-06 03:57:47 -0500

    deleted helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,5 @@
 module ApplicationHelper
 
-def signed_in(hunter)
 
-	session[:hunter_id]= hunter.hunter_id
-
-end
-
-def current_hunter
-
-	@current_hunter ||= Hunter.find(session[:hunter_id]) if session[:hunter_id]
-
-end
-
-def signed_in?
-
-	session[:hunter_id]
-
-end
 
 end
