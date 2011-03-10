92713c336486cfae3970a255f50b0ab3014226cb
Eric <eric@ubuntu.(none)>
2011-03-10 17:16:26 +0800

    Implementing Trialpay - fixed md5

--- app/controllers/trialpay_controller.rb
+++ app/controllers/trialpay_controller.rb
@@ -1,17 +1,20 @@
-require 'md5'
+require 'openssl'
 
 class TrialpayController < ApplicationController
 
 def proc
 
-puts params.inspect
-#puts request.headers.inspect
+	if !validation
+		render :text => "Unauthorized", status => :unauthorized
+		return
+	end
+
 	trialpay_order_id = params[:oid]
 	sid = params[:sid]
 
-	validation
 
-	render :text => 'there is an error'#'1'
+
+	render :text => '1'
 	return
 
 end
@@ -21,10 +24,8 @@ private
 def validation
 
 	expected_sig = request.headers['HTTP_TRIALPAY_HMAC_MD5']
-puts "EXPECTED: "+expected_sig
-puts "QUERY: "+ request.headers['QUERY_STRING']
 
-	sig = MD5.new(request.headers['QUERY_STRING'] + KEY)
+	sig = OpenSSL::HMAC.hexdigest('md5', KEY, request.headers['QUERY_STRING'])
 
 	if expected_sig == sig 
 		return true 
