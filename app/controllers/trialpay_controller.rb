b090d5008f8c4fcf779ff24b71ef7c52b8bf586c
Eric <eric@ubuntu.(none)>
2011-03-10 14:35:27 +0800

    Implementing trialpay

--- app/controllers/trialpay_controller.rb
+++ app/controllers/trialpay_controller.rb
@@ -2,6 +2,18 @@ class TrialpayController < ApplicationController
 
 def process
 
+puts params.inspect
+trialpay_order_id = params[:oid]
+sid = params[:sid]
+
+render :text => '1'
+
+end
+
+private
+
+def validation
+
 end
 
 end
