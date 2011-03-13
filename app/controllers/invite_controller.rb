bebd4d1102a73b87588fefdacc0f3e8259e8f628
Eric <eric@ubuntu.(none)>
2011-03-13 22:24:44 +0800

    fixed closing invitation form

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,8 +3,8 @@ class InviteController < ApplicationController
 	#flash[:notice]=params[:email]
 	#FwbMailer.invite_email(params[:email])
 
-	FwbMailer.invite_email('jscchiu@gmail.com', 'yo').deliver
-	render :nothing => true
+#	FwbMailer.invite_email('jscchiu@gmail.com', 'yo').deliver
+	render :text => "success"
   end
 
   
