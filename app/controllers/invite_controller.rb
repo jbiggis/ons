18a040b55347ea92ed8151d67a2b160a636d9841
Eric <eric@ubuntu.(none)>
2011-03-14 12:43:58 +0800

    fixed fucking curtains

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,9 +3,9 @@ class InviteController < ApplicationController
 	#flash[:notice]=params[:email]
 	#FwbMailer.invite_email(params[:email])
 
-#	FwbMailer.invite_email('jscchiu@gmail.com', 'yo').deliver
-#	current_hunter.invites -= 1
-#	current_hunter.save
+	FwbMailer.invite_email('jscchiu@gmail.com', 'yo').deliver
+	current_hunter.invites -= 1
+	current_hunter.save
 	render :text => "success"
 
   end
