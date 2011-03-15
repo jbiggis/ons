67ca3acc806ba2393b936f5d04618d9dca917636
Jonathan <jonathan@ubuntu.(none)>
2011-03-15 12:40:21 -0400

    changed appid in JS

--- public/javascripts/facebook-init.js
+++ public/javascripts/facebook-init.js
@@ -1,6 +1,7 @@
 
   FB.init({
-	appId  : '202763786417566',
+	//appId  : '202763786417566',
+	appId  : '200426483308435',	
 	status : true, // check login status
 	cookie : true, // enable cookies to allow the server to access the session
 	xfbml  : true  // parse XFBML
