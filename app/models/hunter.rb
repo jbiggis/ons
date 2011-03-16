46c9a709af46403c3ecb12a5dc3a2cbc140f7f55
Jonathan <jonathan@ubuntu.(none)>
2011-03-16 01:04:41 -0400

    added keys to attr_accessible in Hunter

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -2,7 +2,7 @@ class Hunter < ActiveRecord::Base
 
 set_primary_key :hunter_id
 
-attr_accessible :hunter_id, :first_name, :last_name, :email, :gender, :DOB, :reveals, :access_token
+attr_accessible :hunter_id, :first_name, :last_name, :email, :gender, :DOB, :keys, :access_token
 
 has_many :targets, :dependent => :destroy
 has_many :orders, :dependent => :destroy
